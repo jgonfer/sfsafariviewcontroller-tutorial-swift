5d1ab03925a471d7ac0b52954ce64252c77455a4
Josep Gonzalez <josep.gonfer@gmail.com>
2017-03-22 17:53:27 +0100

    Empty project

---
+++ SFSafariVC/SFSafariVC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SFSafariVC
+//
+//  Created by jgonzalez on 22/3/17.
+//  Copyright © 2017 jgonfer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func openSFSafariViewController(_ sender: UIButton) {
+    }
+
+}
+
