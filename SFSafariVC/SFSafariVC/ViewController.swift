128b6a445f4600d5ae93e05c8bdc47a51c69d837
Josep Gonzalez <josep.gonfer@gmail.com>
2017-03-24 17:31:09 +0100

    Added new button to open a browser with Reader mode

--- SFSafariVC/SFSafariVC/ViewController.swift
+++ SFSafariVC/SFSafariVC/ViewController.swift
@@ -10,35 +10,48 @@ import UIKit
 import SafariServices
 
 class ViewController: UIViewController {
-    @IBOutlet weak var openButton: UIButton!
+    @IBOutlet weak var openBrowserButton: UIButton!
+    @IBOutlet weak var openReaderButton: UIButton!
     
     private var urlString:String = "https://www.w3schools.com/js/tryit.asp?filename=tryjs_cookie_username"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        openButton.backgroundColor = .orange
-        openButton.layer.cornerRadius = 10
-        openButton.setTitleColor(.white, for: .normal)
+        openBrowserButton.backgroundColor = .orange
+        openBrowserButton.layer.cornerRadius = 10
+        openBrowserButton.setTitleColor(.white, for: .normal)
+        openReaderButton.backgroundColor = .blue
+        openReaderButton.layer.cornerRadius = 10
+        openReaderButton.setTitleColor(.white, for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func openSFSafariViewController(_ sender: UIButton) {
-        let svc = SFSafariViewController(url: NSURL(string: self.urlString)! as URL)
+    
+    fileprivate func openBrowser(withReaderMode readerMode: Bool) {
+        let svc = SFSafariViewController(url: NSURL(string: self.urlString)! as URL, entersReaderIfAvailable: readerMode)
+        // You can also init the view controller with the SFSafariViewController:url: method
         svc.delegate = self
         if #available(iOS 10.0, *) {
             // The color to tint the background of the navigation bar and the toolbar.
-            svc.preferredBarTintColor = .orange
+            svc.preferredBarTintColor = readerMode ? .blue : .orange
             // The color to tint the the control buttons on the navigation bar and the toolbar.
             svc.preferredControlTintColor = .white
         } else {
             // Fallback on earlier versions
         }
         present(svc, animated: true, completion: nil)
+    }
+
+    @IBAction func openInAppBrowser(_ sender: UIButton) {
+        openBrowser(withReaderMode: false)
+    }
+    
+    @IBAction func openReaderMode(_ sender: UIButton) {
+        openBrowser(withReaderMode: true)
     }
 }
 
